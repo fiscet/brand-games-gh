32aa2bcfe1e111b36a4a6c427fc9cdc8dd9581a4
Christian Zanchetta <>
2023-07-10 16:29:26 +0200

    Minor changes

--- client/src/app/common/global-constants.ts
+++ client/src/app/common/global-constants.ts
@@ -1,5 +1,6 @@
 export class GlobalConstants {
     public static apiEndpoint: string = "https://brandgames.fiscet.it/server/public/api";
+    // public static apiEndpoint: string = "http://localhost:8080/api"; // If the server runs locally
     public static gameImageUrl: string = "https://stage.whgstage.com/scontent/images/games";
       
     public static appTitle: string = "Brand Games";
